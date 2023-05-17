6412fcf38a13fcb3280144fadc2ad5fdc6875fd7
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-05-17 20:39:55 +0530

    make post content bigger

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ const PostView = (props: PostWithUser) => {
             @{author.username} · {dayjs(post.createdAt).fromNow()}
           </span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-xl">{post.content}</span>
       </div>
     </div>
   );
